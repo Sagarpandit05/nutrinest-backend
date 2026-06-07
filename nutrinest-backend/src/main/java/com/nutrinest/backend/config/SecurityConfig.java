1893b5676746c9d09d6225ef990b9da43542bcd0
Sagar <sagarkpandit77@gmail.com>
2026-06-07 19:38:33 +0530

    Fix CORS

--- nutrinest-backend/src/main/java/com/nutrinest/backend/config/SecurityConfig.java
+++ nutrinest-backend/src/main/java/com/nutrinest/backend/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // React
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://nutrinestfrontend.vercel.app")); // React
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
